93d80d2cc0d68e3d36ac4c54fbae9d852b666bdc
LuisaFuentesL <l.fuentesl@uniandes.edu.co>
2023-09-27 02:35:32 -0500

    Vista receta lista

--- app/src/main/java/com/example/cookclock/MainActivity.kt
+++ app/src/main/java/com/example/cookclock/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.cookclock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var receta_boton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_listar_recetas)
+
+        receta_boton = findViewById<Button>(R.id.button2)
+
+        receta_boton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View) {
+                setContentView(R.layout.receta)
+            }
+        })
+
     }
 }
